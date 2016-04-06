12c80f60aab0702ead60953e5258cf380dafc919
ma3100 <masato.syuukatu@gmail.com>
2016-04-06 14:14:25 +0900

    Created by Coding Ground Client

---
+++ New Project-20160406/DoVariable.cs
@@ -0,0 +1,19 @@
+using System.IO;
+using System;
+
+public static class DoVariable
+{
+    public static void Do()
+    {
+        int x = 2;
+        int y = 3;
+        int z = x + y;
+        Console.WriteLine(z);
+        
+        string a = "hamanaka";
+        string b = "fumio";
+        string c = a + b;
+        Console.WriteLine(c);
+
+    }
+}
