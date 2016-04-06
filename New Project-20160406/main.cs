23039fdddc020a2bbb99f6a1c71968cb5388f256
ma3100 <masato.syuukatu@gmail.com>
2016-04-06 09:58:40 +0900

    Created by Coding Ground Client

---
+++ New Project-20160406/main.cs
@@ -0,0 +1,12 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int int = 0;
+        string text = "text"
+        Console.WriteLine("Hello, World!");
+    }
+}
